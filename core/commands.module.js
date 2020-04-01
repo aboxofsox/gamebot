ab3164f3277403258e2e3ae21631875f0253b40b
aboxofsox <idkwthfml@gmail.com>
2020-03-31 22:34:59 -0500

    typo

--- core/commands.module.js
+++ core/commands.module.js
@@ -171,7 +171,7 @@ module.exports = {
         }
         if(command === 'gb-join') {
             if(lfg) {
-                if(lobby.includes(msg.author.username) {
+                if(lobby.includes(msg.author.username)) {
                     return msg.channel.reply(`You can't join the same party twice my dude.`);
                 }
                 msg.channel.send(`${msg.author.username} is down to play.`);
