74d3318810cf42e29f6191896ca4e6312c9d9668
aboxofsox <idkwthfml@gmail.com>
2020-04-22 01:13:13 +0100

    added async function

--- core/new.trivia.js
+++ core/new.trivia.js
@@ -1,8 +1,8 @@
 const api = require('./api.module.js');
 
 module.exports = {
-    draw: () =>{
-        const questions = api.quiz();
+    draw: async () =>{
+        const questions = await api.quiz();
         // const rand = Math.floor(Math.random() * questions.results.length);
 
         console.log(questions);
